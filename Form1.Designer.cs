8b933aab3c6850c82ad6607bf262712bf36088ec
ryuya-kanbara <ryuya06060606@gmail.com>
2020-01-29 17:04:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace GitHub学習用
+{
+    partial class F1000
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.cmbEnzanshi = new System.Windows.Forms.ComboBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnzan1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEnzan = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtValue1.Location = new System.Drawing.Point(25, 65);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(60, 23);
+            this.txtValue1.TabIndex = 0;
+            // 
+            // cmbEnzanshi
+            // 
+            this.cmbEnzanshi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEnzanshi.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbEnzanshi.FormattingEnabled = true;
+            this.cmbEnzanshi.Items.AddRange(new object[] {
+            "",
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbEnzanshi.Location = new System.Drawing.Point(86, 65);
+            this.cmbEnzanshi.Name = "cmbEnzanshi";
+            this.cmbEnzanshi.Size = new System.Drawing.Size(52, 24);
+            this.cmbEnzanshi.TabIndex = 1;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtValue2.Location = new System.Drawing.Point(139, 65);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(60, 23);
+            this.txtValue2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(202, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "=";
+            // 
+            // txtEnzan1
+            // 
+            this.txtEnzan1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtEnzan1.Location = new System.Drawing.Point(219, 65);
+            this.txtEnzan1.Name = "txtEnzan1";
+            this.txtEnzan1.ReadOnly = true;
+            this.txtEnzan1.Size = new System.Drawing.Size(60, 23);
+            this.txtEnzan1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(512, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "計算システム";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEnzan
+            // 
+            this.btnEnzan.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnEnzan.Location = new System.Drawing.Point(25, 32);
+            this.btnEnzan.Name = "btnEnzan";
+            this.btnEnzan.Size = new System.Drawing.Size(81, 29);
+            this.btnEnzan.TabIndex = 6;
+            this.btnEnzan.Text = "計算開始";
+            this.btnEnzan.UseVisualStyleBackColor = true;
+            this.btnEnzan.Click += new System.EventHandler(this.btnEnzan_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClear.Location = new System.Drawing.Point(112, 32);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(81, 29);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "クリア";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.BackColor = System.Drawing.SystemColors.Control;
+            this.btnEnd.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnEnd.Location = new System.Drawing.Point(397, 119);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(103, 29);
+            this.btnEnd.TabIndex = 8;
+            this.btnEnd.Text = "システム終了";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // F1000
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 160);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEnzan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEnzan1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.cmbEnzanshi);
+            this.Controls.Add(this.txtValue1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "F1000";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "F1000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.ComboBox cmbEnzanshi;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnzan1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnzan;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using sd_lib = System_ds_CommonLibraryCS.libCommon_Common;
+
+namespace GitHub学習用
+{
+    public partial class F1000 : Form
+    {
+        public F1000()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 計算ボタンクリック処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnzan_Click(object sender, EventArgs e)
+        {
+            double Value1 = 0;
+            double Value2 = 0;
+
+            double.TryParse(txtValue1.Text, out Value1);
+            double.TryParse(txtValue2.Text, out Value2);
+
+            try
+            {
+                //選択した演算子により演算処理分岐
+                switch(cmbEnzanshi.Text)
+                {
+                    case "+":
+
+                        //足し算
+                        txtEnzan1.Text = (Value1 + Value2).ToString();
+
+                        break;
+
+                    case "-":
+
+                        //引き算
+                        txtEnzan1.Text = (Value1 - Value2).ToString();
+
+                        break;
+
+                    case "*":
+
+                        //掛け算
+                        txtEnzan1.Text = (Value1 * Value2).ToString();
+
+                        break;
+
+                    case "/":
+
+                        //エラー処理追加 2020/01/24
+                        if(Value2== 0)
+                        {
+                            MessageBox.Show("0で除算する事はできません。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        //割算
+                        txtEnzan1.Text = (Value1 / Value2).ToString();
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                sd_lib.ErrorLog_Write("[Object Name]:" + MethodBase.GetCurrentMethod().ReflectedType.Name.ToString() +
+                                    "  [Method Name]:" + MethodBase.GetCurrentMethod().Name +
+                                    "  [Error kind]:Exception Error" + "\r\n" + ex.Message + "\r\n" + ex.StackTrace, true);
+#if DEBUG
+                {
+                    MessageBox.Show(ex.Message, MethodBase.GetCurrentMethod().ReflectedType.Name.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+#endif
+            }
+            finally
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 値クリアボタンクリック処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtValue1.Text = "";
+                txtValue2.Text = "";
+                txtEnzan1.Text = "";
+
+                cmbEnzanshi.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                sd_lib.ErrorLog_Write("[Object Name]:" + MethodBase.GetCurrentMethod().ReflectedType.Name.ToString() +
+                                    "  [Method Name]:" + MethodBase.GetCurrentMethod().Name +
+                                    "  [Error kind]:Exception Error" + "\r\n" + ex.Message + "\r\n" + ex.StackTrace, true);
+#if DEBUG
+                {
+                    MessageBox.Show(ex.Message, MethodBase.GetCurrentMethod().ReflectedType.Name.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+#endif
+            }
+            finally
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// システム終了ボタン処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                sd_lib.ErrorLog_Write("[Object Name]:" + MethodBase.GetCurrentMethod().ReflectedType.Name.ToString() +
+                                    "  [Method Name]:" + MethodBase.GetCurrentMethod().Name +
+                                    "  [Error kind]:Exception Error" + "\r\n" + ex.Message + "\r\n" + ex.StackTrace, true);
+#if DEBUG
+                {
+                    MessageBox.Show(ex.Message, MethodBase.GetCurrentMethod().ReflectedType.Name.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+#endif
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
